08e1ee8e2f8d327c14dab88608ee9a70e444b474
Paul <85436971+Destruc69@users.noreply.github.com>
2024-03-29 18:59:59 +1100

    this

--- src/main/java/paul/fallen/events/AutoJoin.java
+++ src/main/java/paul/fallen/events/AutoJoin.java
@@ -15,21 +15,23 @@ public class AutoJoin {
     @SubscribeEvent
     public void onTick(TickEvent event) {
         if (FALLENClient.INSTANCE.getModuleManager().autoJoin.getState()) {
-            if (Minecraft.getInstance().currentScreen instanceof MultiplayerScreen) {
-                Calendar calendar = Calendar.getInstance();
-                int hour = calendar.get(Calendar.HOUR_OF_DAY);
+            if (Minecraft.getInstance().currentScreen != null) {
+                if (Minecraft.getInstance().currentScreen instanceof MultiplayerScreen) {
+                    Calendar calendar = Calendar.getInstance();
+                    int hour = calendar.get(Calendar.HOUR_OF_DAY);
 
-                double hourValue = FALLENClient.INSTANCE.getModuleManager().autoJoin.hour.dval;
-                int parsedHour = (int) Math.round(hourValue); // Round the double value to the nearest integer
+                    double hourValue = FALLENClient.INSTANCE.getModuleManager().autoJoin.hour.dval;
+                    int parsedHour = (int) Math.round(hourValue); // Round the double value to the nearest integer
 
-                if (Math.round(hour) == Math.round(parsedHour)) {
-                    ((MultiplayerScreen) Minecraft.getInstance().currentScreen).connectToSelected();
+                    if (Math.round(hour) == Math.round(parsedHour)) {
+                        ((MultiplayerScreen) Minecraft.getInstance().currentScreen).connectToSelected();
+                    }
+
+                    int x = 2 + Minecraft.getInstance().fontRenderer.getStringWidth("Join selected server at hour: " + parsedHour);
+                    int y = 10;
+
+                    UIUtils.drawTextOnScreen("Join selected server at hour: " + parsedHour, x, y, new Color(FALLENClient.INSTANCE.getClickgui().textRGB).getRGB());
                 }
-
-                int x = Minecraft.getInstance().getMainWindow().getScaledWidth() + 2 + Minecraft.getInstance().fontRenderer.getStringWidth("Join selected server at hour: " + parsedHour);
-                int y = 10;
-
-                UIUtils.drawTextOnScreen("Join selected server at hour: " + parsedHour, x, y, new Color(FALLENClient.INSTANCE.getClickgui().textRGB).getRGB());
             }
         }
     }
